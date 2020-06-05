e880508c6f3babc88ecb6ea56a62a8b68803e827
hvgazula <hvgazula@umich.edu>
2020-06-05 00:28:06 -0400

    Added GPU related code

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sys
 from datetime import datetime
 
+import torch
+
 from arg_parser import arg_parser
 from config import build_config
 from utils import fix_random_seed
@@ -16,3 +18,19 @@ fix_random_seed(CONFIG)
 
 sys.stdout = open(CONFIG["LOG_FILE"], 'w')
 print(f'Start Time: {date_str}')
+
+# Model objectives
+MODEL_OBJ = {
+    "ConvNet10": "classifier",
+    "PITOM": "classifier",
+    "MeNTALmini": "classifier",
+    "MeNTAL": "seq2seq"
+}
+
+# GPUs
+DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+args.gpus = min(args.gpus, torch.cuda.device_count())
+
+args.model = args.model.split("_")[0]
+classify = False if (args.model in MODEL_OBJ
+                     and MODEL_OBJ[args.model] == "seq2seq") else True
