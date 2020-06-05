adf9e961966d093c2862d6b692c3c23a658c1132
hvgazula <hvgazula@umich.edu>
2020-06-04 22:27:54 -0400

    Added arg_parser.py and config.py

---
+++ arg_parser.py
@@ -0,0 +1,53 @@
+import argparse
+
+
+def arg_parser(default_args=None):
+    ''' Explanation of the Arguments:
+    model (string): Type of model to run from among
+                    (PITOM, ConvNet, MeNTALmini, MeNTAL)
+    subject (list of strings): subject id's as a list
+    shift (integer): Amount by which the onset should be shifted
+    lr (float): learning rate
+    gpus (int): number of gpus for the model to run on
+    epochs (int):
+    batch-size (int):
+    window-size (int): window size to consider for the word in ms
+    bin-size (int): bin size in ms
+    init-model (string):
+'''
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', type=str, default='MeNTAL')
+    parser.add_argument('--subjects', nargs='*', default=['676'])
+    parser.add_argument('--shift', type=int, default=0)
+    parser.add_argument('--lr', type=float, default=1e-4)
+    parser.add_argument('--gpus', type=int, default=4)
+    parser.add_argument('--epochs', type=int, default=50)
+    parser.add_argument('--batch-size', type=int, default=48)
+    parser.add_argument('--window-size', type=int, default=2000)
+    parser.add_argument('--bin-size', type=int, default=50)
+    parser.add_argument('--init-model', type=str, default=None)
+    parser.add_argument('--no-plot', action='store_false', default=False)
+    parser.add_argument('--max-electrodes',
+                        nargs='*',
+                        type=int,
+                        default=[64])
+    parser.add_argument('--vocab-min-freq', type=int, default=1)
+    parser.add_argument('--vocab-max-freq', type=int, default=1000000)
+    parser.add_argument('--max-num-bins', type=int, default=1000000)
+    parser.add_argument('--seed', type=int, default=1234)
+    parser.add_argument('--shuffle', action="store_true", default=False)
+    parser.add_argument('--no-eval', action="store_true", default=False)
+    parser.add_argument('--temp', type=float, default=0.995)
+    parser.add_argument('--tf-dmodel', type=int, default=64)
+    parser.add_argument('--tf-dff', type=int, default=128)
+    parser.add_argument('--tf-nhead', type=int, default=4)
+    parser.add_argument('--tf-nlayer', type=int, default=3)
+    parser.add_argument('--tf-dropout', type=float, default=0.05)
+    parser.add_argument('--weight-decay', type=float, default=0.35)
+
+    if not default_args:
+        args = parser.parse_args()
+    else:
+        args = parser.parse_args(default_args)
+
+    return args
---
+++ config.py
@@ -0,0 +1,97 @@
+import os
+
+from utils import read_file
+
+
+def return_config_dict():
+    '''
+    exclude_words_class: words to be excluded from the classifier vocabulary
+    exclude_words: words to be excluded from the tranformer vocabulary
+    log_interval:
+    '''
+    CONFIG = {
+        "begin_token":
+        "<s>",
+        "datum_suffix": ("conversation_trimmed", "trimmed"),
+        "end_token":
+        "</s>",
+        "exclude_words_class": [
+            "sp", "{lg}", "{ns}", "it", "a", "an", "and", "are", "as", "at",
+            "be", "being", "by", "for", "from", "is", "of", "on", "that",
+            "that's", "the", "there", "there's", "this", "to", "their", "them",
+            "these", "he", "him", "his", "had", "have", "was", "were", "would"
+        ],
+        "exclude_words": ["sp", "{lg}", "{ns}"],
+        "log_interval":
+        32,
+        "data_dir":
+        "/scratch/gpfs/hgazula/brain2en-seq2seq-data",
+        "num_cpus":
+        8,
+        "oov_token":
+        "<unk>",
+        "pad_token":
+        "<pad>",
+        "print_pad":
+        120,
+        "train_convs":
+        '-train-convs.txt',
+        "valid_convs":
+        '-valid-convs.txt',
+        "vocabulary":
+        'std'
+    }
+
+    return CONFIG
+
+
+def build_config(args, results_str):
+
+    CONFIG = return_config_dict()
+
+    # Format directory logistics
+    CONV_DIRS = [
+        CONFIG["data_dir"] + '/%s-conversations/' % i for i in args.subjects
+    ]
+    META_DIRS = [
+        CONFIG["data_dir"] + '/%s-metadata/' % i for i in args.subjects
+    ]
+    SAVE_DIR = './Results/%s-%s-%s/' % (results_str, '+'.join(
+        args.subjects), args.model)
+    LOG_FILE = SAVE_DIR + 'output'
+    if not os.path.isdir(SAVE_DIR):
+        os.makedirs(SAVE_DIR)
+
+    # sys.stdout = open(LOG_FILE, 'a+')
+
+    # Conversation splits
+#     TRAIN_CONV, VALID_CONV = [], []
+#     for meta, subject in zip(META_DIRS, args.subjects):
+#         print(f'Subject: {subject}')
+#         print(f'Training Data:: ', end='')
+#         TRAIN_CONV.append(
+#             read_file("%s%s%s" % (meta, subject, CONFIG["train_convs"])))
+#         print(f'Validation Data:: ', end='')
+#         VALID_CONV.append(
+#             read_file("%s%s%s" % (meta, subject, CONFIG["valid_convs"])))
+
+    DIR_DICT = dict(CONV_DIRS=CONV_DIRS,
+                    META_DIRS=META_DIRS,
+                    SAVE_DIR=SAVE_DIR,
+                    LOG_FILE=LOG_FILE)
+
+    CONFIG.update(DIR_DICT)
+
+    if len(args.subjects) == 1:
+        if args.subjects[0] == '625':
+            CONFIG["datum_suffix"] = [CONFIG["datum_suffix"][0]]
+        elif args.subjects[0] == '676':
+            CONFIG["datum_suffix"] = [CONFIG["datum_suffix"][1]]
+
+    CONFIG.update(vars(args))
+
+    CONFIG["electrode_list"] = [
+        range(1, k + 1) for k in CONFIG["max_electrodes"]
+    ]
+
+    return CONFIG
