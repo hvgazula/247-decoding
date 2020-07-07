abf6883721e22c3eb24bac023fbe2791e8b6d248
hvgazula <hvgazula@umich.edu>
2020-07-07 18:47:12 -0400

    Added 4 gpus to submit.sh

--- submit.sh
+++ submit.sh
@@ -7,7 +7,7 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=16G
 #SBATCH --time=0-02:00:00
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:4
 #SBATCH --mail-type=begin
 #SBATCH --mail-type=fail
 #SBATCH --mail-type=end
