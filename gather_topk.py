6594e8eca015ee10e9c7b5fa6fee96f3f1bd9ce0
hvgazula <hvgazula@umich.edu>
2020-08-17 10:24:23 -0400

    Corrected code to get vocab size from 676
    presence of bad conversations in output gave wrong results

--- gather_topk.py
+++ gather_topk.py
@@ -6,7 +6,7 @@ import numpy as np
 import pandas as pd
 
 PRJCT_FOLDER = os.getcwd()
-EXP_FOLDER = 'experiments-pitom'
+EXP_FOLDER = 'experiments-unequal-window-676'
 PARENT_DIR = os.path.join(PRJCT_FOLDER, EXP_FOLDER)
 
 
@@ -23,6 +23,29 @@ def average_trials(item):
     array = np.array(item)
     mean_array = np.mean(array, axis=0)
     return mean_array.tolist()
+
+
+def return_class_size(output_file_path):
+    with open(output_file_path, 'r') as file_h:
+        for line in file_h:
+            if 'Vocabulary size' in line:
+                n_classes = int(line.split(':')[-1].lstrip())
+                return n_classes
+
+
+def return_avg_auc(auc_file_path):
+    with open(auc_file_path, 'r') as file_h:
+        avg_auc = json.load(file_h)['rocauc_w_avg']
+    return avg_auc
+
+
+def return_topk_list(topk_file_path):
+    with open(topk_file_path, 'r') as file_h:
+        topk_lines = file_h.readlines()[2:5]
+        flat_list = flatten_list(
+            [extract_floats(line.split('\t')[1]) for line in topk_lines])
+
+    return flat_list
 
 
 def whatever_function(word_type, topk_file, auc_summary):
@@ -55,22 +78,17 @@ def whatever_function(word_type, topk_file, auc_summary):
         dict_list.append(dict(zip(arg_strings, exp_config)))
         abc, jkl, nclass = [], [], []
         full_folder_path = os.path.join(PARENT_DIR, folder)
-        trials = os.listdir(full_folder_path)
+        trials = sorted(os.listdir(full_folder_path))
         for trial in trials:
             topk_file_path = os.path.join(full_folder_path, trial,
                                           TOPK_FILE_NAME)
             auc_file_path = os.path.join(full_folder_path, trial, AUC_SUMMARY)
             output_file_path = os.path.join(full_folder_path, trial, 'output')
-            with open(topk_file_path, 'r') as file_h:
-                topk_lines = file_h.readlines()[2:5]
-            flat_list = flatten_list(
-                [extract_floats(line.split('\t')[1]) for line in topk_lines])
-            with open(auc_file_path, 'r') as file_h:
-                avg_auc = json.load(file_h)['rocauc_w_avg']
-            with open(output_file_path, 'r') as file_h:
-                for idx, line in enumerate(file_h):
-                    if idx == 14:
-                        n_classes = int(line.split(':')[-1].lstrip())
+
+            flat_list = return_topk_list(topk_file_path)
+            avg_auc = return_avg_auc(auc_file_path)
+            n_classes = return_class_size(output_file_path)
+
             jkl.append(avg_auc)
             abc.append(flat_list)
             nclass.append(n_classes)
