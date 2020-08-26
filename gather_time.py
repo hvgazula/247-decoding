b4d47c5ae48df2bac57b12ea2310cd5a25ec8104
hvgazula <hvgazula@umich.edu>
2020-08-26 14:54:00 -0400

    Refactored gather_time.py

--- gather_time.py
+++ gather_time.py
@@ -1,21 +1,44 @@
 import os
+import sys
 
 PRJCT_FOLDER = os.getcwd()
-EXP_FOLDER = 'Experiments_Wednesday'
+EXP_FOLDER = sys.argv[1]
 PARENT_DIR = os.path.join(PRJCT_FOLDER, EXP_FOLDER)
 
-folder_list_file = os.path.join(PARENT_DIR, 'folder_list')
 
-with open(folder_list_file, 'r') as file_h:
-    folder_list = file_h.readlines()
+def return_folders():
+    folder_list_file = os.path.join(PARENT_DIR, 'folder_list')
 
-folder_list = [item.rstrip() for item in folder_list]
+    with open(folder_list_file, 'r') as file_h:
+        folder_list = file_h.readlines()
 
-for folder in folder_list:
-    full_folder_path = os.path.join(PARENT_DIR, folder)
-    trials = os.listdir(full_folder_path)
-    for trial in trials:
-        with open(os.path.join(full_folder_path, trial, 'output'), 'r') as file_h: 
-            lines = file_h.readlines()
+    return [item.rstrip() for item in folder_list]
 
-    print(lines[-1])
+
+def read_output(full_folder_path, trial):
+    output_file = os.path.join(full_folder_path, trial, 'output')
+    with open(output_file, 'r') as file_h:
+        lines = file_h.readlines()
+    return lines[-1]
+
+
+def extract_time(time_line):
+    if 'runtime' in time_line:
+        print(time_line.strip().split(' ')[2])
+
+
+def main():
+    folder_list = return_folders()
+    for folder in folder_list:
+        full_folder_path = os.path.join(PARENT_DIR, folder)
+        try:
+            trials = os.listdir(full_folder_path)
+            for trial in trials:
+                time_line = read_output(full_folder_path, trial)
+                extract_time(time_line)
+        except FileNotFoundError:
+            pass
+
+
+if __name__ == '__main__':
+    main()
