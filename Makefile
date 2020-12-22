98ee4445395fd05aa86e9ca75d4cca89153e2015
Zaid Kokaja <zkokaja@gmail.com>
2020-12-22 15:40:49 -0500

    Add and use args, add all 5 folds

--- Makefile
+++ Makefile
@@ -1,8 +1,12 @@
 CMD := echo
+CMD := sbatch submit1.sh
 CMD := python
+
+USR := $(shell whoami | head -c 2)
 
 SID := 625
 MEL := 500
+
 MINF := 30
 # setting a very large number for MEL
 # will extract all common electrodes across all conversations
@@ -15,3 +19,30 @@ create-pickle:
 
 upload-pickle: create-pickle
 	gsutil -m cp -r $(SID)*.pkl gs://247-podcast-data/247_pickles/
+
+
+NL = $(shell expr $(words $(LAGS)) - 1)
+LAGS := $(shell echo {-2048..2048..256})
+LAGS := 0
+
+MODES := prod comp
+MODES := comp
+MODES := prod
+
+run-decoding:
+	for lag in $(LAGS); do \
+	    $(CMD) tfsdec_main.py \
+		--subject $(SID) \
+		--lag $$lag \
+		--fine-epochs 1 \
+		--model test-s_$(SID)-e_64-u_$(USR)-l_$$lag; \
+	done
+
+run-decoding2:
+	for mode in $(MODES); do \
+	    sbatch --array=0-$(NL) submit1.sh tfsdec_main.py \
+		--signal-pickle $(SID)_binned_signal.pkl \
+		--label-pickle $(SID)_$${mode}_labels_MWF30.pkl \
+		--lags $(LAGS) \
+		--model s_$(SID)-m_$$mode-e_64-u_$(USR); \
+	done
--- tfsdec_main.py
+++ tfsdec_main.py
@@ -6,6 +6,7 @@ Author: Harshvardhan Gazula
 
 Copyright (c) 2020 Your Company
 '''
+import uuid
 import argparse
 import json
 import os
@@ -41,27 +42,52 @@ def set_seed():
 
     # The below set_seed() will make random number generation
     # in the TensorFlow backend have a well-defined initial state.
-    # For further details, see:
+    # For further details, see
     # https://www.tensorflow.org/api_docs/python/tf/random/set_seed
     tf.random.set_seed(1234)
 
 
 def arg_parser():
-    args = argparse.Namespace()
-    args.patience = 150
-    args.verbose = 1
-    args.conv_filters = 128
-    args.reg = 0.35
-    args.dropout = 0.2
-    args.reg_head = 0
-    args.lr = 0.00025
-    args.fine_epochs = 1000
-    args.lm_head = 0
-    args.seed = 0
-    args.batch_size = 1024
-    args.model = '247_decoding_test'
-    args.lag = -160
-    args.half_window = 512
+    parser = argparse.ArgumentParser()
+
+    # Data args
+    parser.add_argument('--lag', type=int, default=None, help='')
+    parser.add_argument('--lags', type=int, nargs='+', default=None, help='')
+    parser.add_argument('--signal-pickle', type=str, required=True, help='')
+    parser.add_argument('--label-pickle', type=str, required=True, help='')
+    parser.add_argument('--half-window', type=int, default=512, help='')
+
+    # Training args
+    parser.add_argument('--lr', type=float, default=0.01, help='Optimizer learning rate.')
+    parser.add_argument('--batch_size', type=int, default=512, help='Integer or None. Number of samples per gradient update.')
+    parser.add_argument('--fine-epochs', type=int, default=1000, help='Integer. Number of epochs to train the model. An epoch is an iteration over the entire x and y data provided.')
+    parser.add_argument('--patience', type=int, default=150, help='Number of epochs with no improvement after which training will be stopped.')
+    parser.add_argument('--lm_head', action='store_true', help='NotImplementedError')
+
+    # Model definition
+    parser.add_argument('--conv_filters', type=int, default=128, help='Number of convolutional filters in the model.')
+    parser.add_argument('--reg', type=float, default=0.35, help='Float. L2 regularization factor for convolutional layers.')
+    parser.add_argument('--reg_head', type=float, default=0, help='Float. L2 regularization factor for dense head.')
+    parser.add_argument('--dropout', type=float, default=0.2, help='Float between 0 and 1. Fraction of the input units to drop.')
+
+    # Other args
+    parser.add_argument('--model', type=str, default='default-out', help='Name of output directory.')
+    parser.add_argument('--seed', type=int, default=None, help='Random seed.')
+    parser.add_argument('--verbose', type=int, default=2, help='0, 1, or 2. Verbosity mode. 0 = silent, 1 = progress bar, 2 = one line per epoch.')
+
+    args = parser.parse_args()
+
+    if args.lag is None:
+        if os.environ.get('SLURM_ARRAY_TASK_ID') is not None:
+            idx = int(os.environ.get('SLURM_ARRAY_TASK_ID'))
+            assert len(args.lags) > 0
+            assert idx < len(args.lags)
+
+            args.lag = args.lags[idx]
+            # args.model += f'-l_{args.lag}'
+            print(f'Using slurm array lag: {args.lag}')
+        else:
+            args.lag = 0  # default
 
     # lags = np.arange(-1000, 1001, 100).tolist()
     # lags = np.arange(-800, 8001, 32).tolist()
@@ -69,11 +95,11 @@ def arg_parser():
     return args
 
 
-def load_pickles():
-    with open('625_binned_signal.pkl', 'rb') as fh:
+def load_pickles(args):
+    with open(args.signal_pickle, 'rb') as fh:
         signal_d = pickle.load(fh)
 
-    with open('625_both_labels_MWF30.pkl', 'rb') as fh:
+    with open(args.label_pickle, 'rb') as fh:
         label_folds = pickle.load(fh)
 
     print('Signals pickle info')
@@ -249,173 +275,204 @@ if __name__ == '__main__':
     args = arg_parser()
 
     # Logistical things
-    save_dir = os.path.join(os.getcwd(), args.model) + '/'
-    if not os.path.isdir(save_dir):
-        os.mkdir(save_dir)
+    nonce = uuid.uuid4().hex  # TODO - include sbatch job info?
+    save_dir = os.path.join('results', args.model, str(args.lag), nonce) + '/'
+    os.makedirs(save_dir, exist_ok=True)
 
-    signals, stitch_index, label_folds = load_pickles()
+    print(args)
+    with open(os.path.join(save_dir, 'args.json'), 'w') as fp:
+        json.dump(vars(args), fp, indent=4)
 
-    train_fold = [
-        example for example in label_folds if example['fold0'] == 'train'
-    ]
-    dev_fold = [
-        example for example in label_folds if example['fold0'] == 'dev'
-    ]
-    test_fold = [
-        example for example in label_folds if example['fold0'] == 'test'
-    ]
-
-    # Decoding starts here
-    x_train, w_train = extract_signal_from_fold(train_fold, stitch_index, args)
-    x_dev, w_dev = extract_signal_from_fold(dev_fold, stitch_index, args)
-    x_test, w_test = extract_signal_from_fold(test_fold, stitch_index, args)
-
-    # Determine indexing
-    word2index = {w: i for i, w in enumerate(sorted(set(w_train.tolist())))}
-    index2word = {i: word for word, i in word2index.items()}
-
-    y_train = np.array([word2index[w] for w in w_train])
-    y_dev = np.array([word2index[w] for w in w_dev])
-    y_test = np.array([word2index[w] for w in w_test])
-
-    n_classes = np.unique(y_train).size
-
-    print('X train, dev, test:', x_train.shape, x_dev.shape, x_test.shape)
-    print('Y train, dev, test:', y_train.shape, y_dev.shape, y_test.shape)
-    print('W train, dev, test:', w_train.shape, w_dev.shape, w_test.shape)
-    # print('Z train, dev, test:', z_train.shape, z_dev.shape, z_test.shape)
-    print('n_classes:', n_classes,
-          np.unique(y_dev).size,
-          np.unique(y_test).size)
-
-    model = pitom([x_train.shape[1:]], n_classes=None)
-    optimizer = Adam(lr=args.lr)
-    model.compile(loss='mse',
-                  optimizer=optimizer,
-                  metrics=[tf.keras.metrics.CosineSimilarity()])
-
-    # model.summary()
-
-    # -------------------------------------------------------------------------
-    # >> Classification training
-    # -------------------------------------------------------------------------
-    corrs = {}
-    test_result = {}
-    main_history = {}
-
+    signals, stitch_index, label_folds = load_pickles(args)
     histories = []
     test_results = []
     fold_results = []
-    i = 0
-    # Add the decoder, LM head or just a new layer
-    if args.fine_epochs > 0:
-        print('I am inside fine_epochs')
-        model2 = Model(inputs=model.input, outputs=get_decoder()(model.output))
-        model2.compile(
-            loss='categorical_crossentropy',
-            optimizer=optimizer,
-            metrics=[
-                tf.keras.metrics.CategoricalAccuracy(name='top1'),
-                tf.keras.metrics.TopKCategoricalAccuracy(k=5, name='top5'),
-                tf.keras.metrics.TopKCategoricalAccuracy(k=10, name='top10'),
-                tf.keras.metrics.AUC(name='auc'),
-                tf.keras.metrics.Recall(name='recall'),
-                tf.keras.metrics.Precision(name='precision')
-            ])
 
-        with open(os.path.join(save_dir, 'model2-summary.txt'), 'w') as f:
-            with redirect_stdout(f):
-                model2.summary()
+    # TODO - do all folds.
+    for i in range(5):
+        print(f'Running fold {i}')
 
-        stopper = EarlyStopping(monitor='val_top1',
-                                mode='max',
-                                patience=args.patience,
-                                restore_best_weights=True,
-                                verbose=args.verbose)
+        train_fold = [
+            example for example in label_folds if example[f'fold{i}'] == 'train'
+        ]
+        dev_fold = [
+            example for example in label_folds if example[f'fold{i}'] == 'dev'
+        ]
+        test_fold = [
+            example for example in label_folds if example[f'fold{i}'] == 'test'
+        ]
 
-        history = model2.fit(
-            x=x_train,
-            y=to_categorical(y_train, n_classes),
-            epochs=args.fine_epochs,
-            batch_size=args.batch_size,
-            validation_data=[x_dev, to_categorical(y_dev, n_classes)],
-            callbacks=[stopper],
-            verbose=args.verbose)
+        # Decoding starts here
+        x_train, w_train = extract_signal_from_fold(train_fold, stitch_index, args)
+        x_dev, w_dev = extract_signal_from_fold(dev_fold, stitch_index, args)
+        x_test, w_test = extract_signal_from_fold(test_fold, stitch_index, args)
 
-        main_history.update(history.history)
+        # Determine indexing
+        word2index = {w: j for j, w in enumerate(sorted(set(w_train.tolist())))}
+        index2word = {j: word for word, j in word2index.items()}
 
-    histories.append(main_history)
+        y_train = np.array([word2index[w] for w in w_train])
+        y_dev = np.array([word2index[w] for w in w_dev])
+        y_test = np.array([word2index[w] for w in w_test])
 
-    res, res2 = {}, {}
-    if args.lm_head or args.fine_epochs > 0:
-        # Evaluate end to end on test set
-        testset = model2.evaluate(x_test,
-                                  to_categorical(y_test, n_classes),
-                                  verbose=args.verbose)
-        test_result2 = {
-            metric: float(result)
-            for metric, result in zip(model2.metrics_names, testset)
-        }
-        test_result.update(test_result2)
+        n_classes = np.unique(y_train).size
 
-        test_results.append(test_result)
+        print('X train, dev, test:', x_train.shape, x_dev.shape, x_test.shape)
+        print('Y train, dev, test:', y_train.shape, y_dev.shape, y_test.shape)
+        print('W train, dev, test:', w_train.shape, w_dev.shape, w_test.shape)
+        # print('Z train, dev, test:', z_train.shape, z_dev.shape, z_test.shape)
+        print('n_classes:', n_classes,
+              np.unique(y_dev).size,
+              np.unique(y_test).size)
 
-        # Prune predictions to only ones that are in w_test, create a mapping
-        # of i2w and recreate y_test.
-        predictions = model2.predict(x_test)
-        if args.lm_head:
-            test_vocab = sorted(set(w_test))
-            test_indices = [word2index[w] for w in test_vocab]
-            test_w2i = {w: i for i, w in enumerate(test_vocab)}
-            test_i2w = {i: w for w, i in test_w2i.items()}
+        model = pitom([x_train.shape[1:]], n_classes=None)
+        optimizer = Adam(lr=args.lr)
+        model.compile(loss='mse',
+                      optimizer=optimizer,
+                      metrics=[tf.keras.metrics.CosineSimilarity()])
 
-            predictions = predictions[:, test_indices]
-            y_test_pruned = [test_w2i[w] for w in w_test]
-            y_train_freq = Counter(y_test_pruned)
+        # model.summary()
+
+        # -------------------------------------------------------------------------
+        # >> Classification training
+        # -------------------------------------------------------------------------
+        corrs = {}
+        test_result = {}
+        main_history = {}
+        loaded_model = False  # TODO - make an arg appropriately
+
+        # Add the decoder, LM head or just a new layer
+        if args.fine_epochs > 0:
+            print('I am inside fine_epochs')
+            model2 = Model(inputs=model.input, outputs=get_decoder()(model.output))
+            model2.compile(
+                loss='categorical_crossentropy',
+                optimizer=optimizer,
+                metrics=[
+                    tf.keras.metrics.CategoricalAccuracy(name='top1'),
+                    tf.keras.metrics.TopKCategoricalAccuracy(k=5, name='top5'),
+                    tf.keras.metrics.TopKCategoricalAccuracy(k=10, name='top10'),
+                    tf.keras.metrics.AUC(name='auc'),
+                    tf.keras.metrics.Recall(name='recall'),
+                    tf.keras.metrics.Precision(name='precision')
+                ])
+
+            with open(os.path.join(save_dir, 'model2-summary.txt'), 'w') as fp:
+                with redirect_stdout(fp):
+                    model2.summary()
+
+            stopper = EarlyStopping(monitor='val_top1',
+                                    mode='max',
+                                    patience=args.patience,
+                                    restore_best_weights=True,
+                                    verbose=args.verbose)
+
+            history = model2.fit(
+                x=x_train,
+                y=to_categorical(y_train, n_classes),
+                epochs=args.fine_epochs,
+                batch_size=args.batch_size,
+                validation_data=[x_dev, to_categorical(y_dev, n_classes)],
+                callbacks=[stopper],
+                verbose=args.verbose)
+
+            model2.save(os.path.join(save_dir, f'model2-fold{i}.h5'))
+
+            main_history.update(history.history)
         else:
-            y_train_freq = Counter(y_train)
-            y_test_pruned = y_test
-            test_i2w = index2word
+            trained_model_fn = os.path.join(save_dir, f'model2-fold{i}.h5')
+            if os.path.isfile(trained_model_fn):
+                print('Loading model!')
+                loaded_model = True
+                model2 = tf.keras.models.load_model(trained_model_fn)
 
-        res = evaluate_topk(predictions,
-                            to_categorical(y_test_pruned,
-                                           predictions.shape[1]),
-                            test_i2w,
-                            y_train_freq,
-                            save_dir,
-                            min_train=0,
-                            prefix='eval_',
-                            suffix=f'-fold_{i}-test',
-                            title=args.model)
+        histories.append(main_history)
 
-        res2 = evaluate_roc(predictions,
-                            to_categorical(y_test_pruned,
-                                           predictions.shape[1]),
-                            test_i2w,
-                            y_train_freq,
-                            save_dir,
-                            min_train=0,
-                            suffix=f'-fold_{i}-test',
-                            title=args.model)
+        res, res2 = {}, {}
+        if args.lm_head or args.fine_epochs > 0 or loaded_model:
+            # Evaluate end to end on test set
+            testset = model2.evaluate(x_test,
+                                      to_categorical(y_test, n_classes),
+                                      verbose=args.verbose)
+            test_result2 = {
+                metric: float(result)
+                for metric, result in zip(model2.metrics_names, testset)
+            }
+            test_result.update(test_result2)
 
-        # Just to check
-        guesses = predictions.argmax(axis=-1)
-        accuracy = (guesses == y_test_pruned).sum() / len(y_test_pruned)
-        res['manual_top1'] = accuracy
+            test_results.append(test_result)
 
-    # Store final value of each dev metric, then test metrics
-    values = {k: float(v[-1]) for k, v in main_history.items()}
-    values.update({f'test_{metric}': v for metric, v in test_result.items()})
-    values.update(corrs)  # add our metrics
+            # Prune predictions to only ones that are in w_test, create a mapping
+            # of i2w and recreate y_test.
+            predictions = model2.predict(x_test)
+            if args.lm_head:
+                test_vocab = sorted(set(w_test))
+                test_indices = [word2index[w] for w in test_vocab]
+                test_w2i = {w: i for i, w in enumerate(test_vocab)}
+                test_i2w = {i: w for w, i in test_w2i.items()}
 
-    values['n_classes'] = np.unique(y_train).size
-    values['n_classes_dev'] = np.unique(y_dev).size
-    values['n_classes_test'] = np.unique(y_test).size
-    values['n_train'] = x_train.shape[0]
-    values['n_dev'] = x_dev.shape[0]
-    values['n_test'] = x_test.shape[0]
-    values.update(res)
-    values.update(res2)
+                predictions = predictions[:, test_indices]
+                y_test_pruned = [test_w2i[w] for w in w_test]
+                y_train_freq = Counter(y_test_pruned)
+            else:
+                y_train_freq = Counter(y_train)
+                y_test_pruned = y_test
+                test_i2w = index2word
 
-    fold_results.append(values)
-    print(json.dumps(values, indent=2))
+            res = evaluate_topk(predictions,
+                                to_categorical(y_test_pruned,
+                                               predictions.shape[1]),
+                                test_i2w,
+                                y_train_freq,
+                                save_dir,
+                                min_train=0,
+                                prefix='eval_',
+                                suffix=f'-fold_{i}-test',
+                                title=args.model)
+
+            res2 = evaluate_roc(predictions,
+                                to_categorical(y_test_pruned,
+                                               predictions.shape[1]),
+                                test_i2w,
+                                y_train_freq,
+                                save_dir,
+                                min_train=0,
+                                suffix=f'-fold_{i}-test',
+                                title=args.model)
+
+            # Just to check
+            guesses = predictions.argmax(axis=-1)
+            accuracy = (guesses == y_test_pruned).sum() / len(y_test_pruned)
+            res['manual_top1'] = accuracy
+
+        # Store final value of each dev metric, then test metrics
+        values = {k: float(v[-1]) for k, v in main_history.items()}
+        values.update({f'test_{metric}': v for metric, v in test_result.items()})
+        values.update(corrs)  # add our metrics
+
+        values['n_classes'] = np.unique(y_train).size
+        values['n_classes_dev'] = np.unique(y_dev).size
+        values['n_classes_test'] = np.unique(y_test).size
+        values['n_train'] = x_train.shape[0]
+        values['n_dev'] = x_dev.shape[0]
+        values['n_test'] = x_test.shape[0]
+        values.update(res)
+        values.update(res2)
+
+        fold_results.append(values)
+        print(json.dumps(values, indent=2))
+
+    # TODO - plot loss curves
+
+    # Save all metrics
+    results = {}
+    for metric in fold_results[0]:
+        results[f'avg_{metric}'] = np.mean([tr[metric] for tr in fold_results])
+
+    print(json.dumps(results, indent=2))
+
+    results['runs'] = fold_results
+    results['args'] = vars(args)
+
+    with open(os.path.join(save_dir, 'results.json'), 'w') as fp:
+        json.dump(results, fp, indent=4)
